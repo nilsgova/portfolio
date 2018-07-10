2cf2d46371c0e173924e2c8b5036995026ded905
nilsgova <nils.govaerts@gmail.com>
2018-07-10 07:09:47 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(e) {
+
+    $('.menu-toggle').click(function() {
+
+        $('.site-nav').toggleClass('site-nav--open', 500);
+        $(this).toggleClass('open');
+
+    })
+
+
+
+
+});
