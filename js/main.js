290cc72d405eca6c93e11d352abafa33493e1942
nilsgova <nils.govaerts@gmail.com>
2018-07-12 07:28:59 +0200

    about
    addes start of about page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(document).ready(function(e) {
 
+// Hamburger Menu //
+
     $('.menu-toggle').click(function() {
 
         $('.site-nav').toggleClass('site-nav--open', 500);
         $(this).toggleClass('open');
-
     })
-
-
-
 
 });
