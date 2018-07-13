9a4075476c72ba22c03527d038bd5309087dff4e
nilsgova <nils.govaerts@gmail.com>
2018-07-13 09:31:08 +0200

    skills update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function(e) {
+$(document).ready(function (e) {
 
 // Hamburger Menu //
 
-    $('.menu-toggle').click(function() {
+    $('.menu-toggle').click(function () {
 
         $('.site-nav').toggleClass('site-nav--open', 500);
         $(this).toggleClass('open');
     })
+
+    // SKILLBAR //
 
 });
