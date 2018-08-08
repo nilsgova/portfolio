f958f6c1ea324862027b325802dff72897f83fdd
nilsgova <nils.govaerts@gmail.com>
2018-08-08 22:49:20 +0200

    new menu
    new menu and scroll to top button

--- js/main.js
+++ js/main.js
@@ -2,12 +2,28 @@ $(document).ready(function (e) {
 
 // Hamburger Menu //
 
-    $('.menu-toggle').click(function () {
+        $('.menu-toggle').click(function() {
+            $('nav').toggleClass('active')
+        });
 
-        $('.site-nav').toggleClass('site-nav--open');
-        $(this).toggleClass('open');
-    })
+        $('#nav').click(function() {
+            $(this).siblings().removeClass('active');
+            $(this).toggleClass('active');
+        });
 
-    // SKILLBAR //
+
+    // BACK TO TOP //
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);
+    });
 
 });
